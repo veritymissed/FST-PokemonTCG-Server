45096a313c20b26d8f29f944e36a4011cf9f5daa
Verity Tsai <veritymissed@gmail.com>
2021-12-13 14:05:02 +0800

    feat(dependencies): added helmet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
   const configService = app.get(ConfigService);
+  app.use(helmet());
   await app.listen(configService.get('port'));
 }
 bootstrap();
