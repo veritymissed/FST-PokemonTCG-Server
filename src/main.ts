453311396f1c7c49bfb074915795042589663191
Verity Tsai <veritymissed@gmail.com>
2021-12-13 14:31:50 +0800

    feat(dependencies): added csrf protection

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
   const configService = app.get(ConfigService);
   app.use(helmet());
+  app.use(cookieParser());
+  app.use(csurf());
   await app.listen(configService.get('port'));
 }
 bootstrap();
