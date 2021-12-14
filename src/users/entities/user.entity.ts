6d3da64ecedad16bad75cfcff2da4ad9fe4cda1b
Verity Tsai <veritymissed@gmail.com>
2021-12-14 09:33:57 +0800

    chore(UserEntity): added the role column

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,14 +1,19 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+export enum UserRole {
+  USER = 'user',
+  ADMIN = 'admin',
+}
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
-  @Column({ unique: true })
+  @Column({ unique: true, nullable: false })
   email: string;
 
-  @Column()
+  @Column({ nullable: false })
   password: string;
 
   @Column({
@@ -17,4 +22,12 @@ export class User {
     default: []
   })
   favorite_cards: [];
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    nullable: false,
+    default: UserRole.USER
+  })
+  role: string;
 }
